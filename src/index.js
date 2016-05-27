53b5153380af944d30e1cc72f0f53d0eaa590062
Jared Grippe <jared@learnup.com>
2016-05-27 12:47:15 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,56 +1,60 @@
-export default class Putio {
+const URI = require('urijs');
+// const request = require('request-promise');
 
+export default class Putio {
+  constructor(options) {
+    this.clientId      = options.clientId;
+    this.secret        = options.secret;
+    this.oauthToken    = options.oauthToken;
+    this.redirectURI   = options.redirectURI;
+    this.accessToken   = options.accessToken;
+    this.oAuthResponseType = options.oAuthResponseType || 'token';
+  }
+
+  oauthUrl(){
+    return URI('https://api.put.io/v2/oauth2/authenticate').query({
+      client_id: this.clientId,
+      response_type: this.oAuthResponseType,
+      redirect_uri: this.redirectURI,
+    })
+  }
+
+  get(url, query) {
+    query = query || {};
+    console.log('GET', url, query);
+    query.oauth_token = this.token;
+    url = URI(url).query(query).toString();
+    return request.get(url).then(JSON.parse);
+  }
+
+  accountInfo(){
+    return this.get(apiURI('/v2/account/info'));
+  }
+
+  addTransfer(magnetLink){
+    return this.post(apiURI('/v2/transfers/add'), {url: magnetLink}).then(function(response){
+      return response.transfer;
+    });
+  }
+
+  transfers(){
+    return this.get(apiURI('/v2/transfers/list')).then(function(response){
+      return response.transfers;
+    });
+  }
 }
 
-// const URI = require('urijs');
-// // const request = require('request-promise');
+const uri = (endpoint, path, query) => {
+  return URI(endpoint).query(query||{}).path(path||'/').toString();
+};
 
-// export default class Putio {
-//   constructor(token) {
-//     this.setToken(token);
-//   }
+const ENDPOINT = 'https://put.io';
+const baseURI = (path, query) => {
+  return uri(ENDPOINT, path, query);
+};
 
-//   setToken(token) {
-//     this.token = token;
-//     return this;
-//   }
-
-//   get(url, query) {
-//     query = query || {};
-//     console.log('GET', url, query);
-//     query.oauth_token = this.token;
-//     url = URI(url).query(query).toString();
-//     return request.get(url).then(JSON.parse);
-//   }
-
-//   accountInfo(){
-//     return this.get(apiURI('/v2/account/info'));
-//   }
-
-//   addTransfer(magnetLink){
-//     return this.post(apiURI('/v2/transfers/add'), {url: magnetLink}).then(function(response){
-//       return response.transfer;
-//     });
-//   }
-
-//   transfers(){
-//     return this.get(apiURI('/v2/transfers/list')).then(function(response){
-//       return response.transfers;
-//     });
-//   }
-// }
-
-// const uri = (endpoint, path, query) => {
-//   return URI(endpoint).query(query||{}).path(path||'/').toString();
-// };
-
-// const ENDPOINT = 'https://put.io';
-// const baseURI = (path, query) => {
-//   return uri(ENDPOINT, path, query);
-// };
-
-// const API_ENDPOINT = 'https://api.put.io/v2';
-// const apiURI = (path, query) => {
-//   return uri(API_ENDPOINT, path, query);
-// };
+const API_ENDPOINT = 'https://api.put.io/v2';
+const apiURI = (path, query) => {
+  return uri(API_ENDPOINT, path, query);
+};
 
